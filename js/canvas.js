4a6a9ec970e3b1ae39281c2836f057ef60ae39de
so-hard <qq1136339397@gmail.com>
2019-06-12 14:43:35 +0800

    refactor: refactor creatGrid function

--- js/canvas.js
+++ js/canvas.js
@@ -1,16 +1,20 @@
 let canvas = document.getElementById("canvas"),
 cxt = canvas.getContext("2d");
 const rebaseWidth = 20;
+const GridArray = [10,10]
 
 
-
-createGrid();
-function createGrid() {
-  for (let i = 0; i < 10; i++) {
-    for (let j = 0; j < 10; j++) {
+function createGrid(array) {
+  let [xGrid,yGrid] = array
+  canvas.width = xGrid*rebaseWidth;
+  canvas.height = yGrid*rebaseWidth;
+  for (let i = 0; i < xGrid ; i++) {
+    for (let j = 0; j <yGrid; j++) {
       cxt.fillStyle = "#000000";
-      cxt.strokeRect(j * rebaseWidth, i * rebaseWidth, rebaseWidth, rebaseWidth);
+      cxt.strokeRect(i*rebaseWidth, j*rebaseWidth, rebaseWidth, rebaseWidth);
     }
   }
 }
+
+createGrid(GridArray);
 
